7868bf3914adb3ac63926053398af21b50e161b7
Felipe Henriquez <felipe.henriquez@uc.cl>
2023-05-22 16:45:57 -0400

    Update: Set up basic react
    agregado routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React from "react";
+import Routing from "./Routing";
+
 
 import "./App.css";
-import UsersListsFromAPI from "./components/usersListsFromAPI";
-import ButtonChangeTheme from "./components/buttonChangeTheme";
+
 
 function App() {
   return (
     <div className="App">
-      <UsersListsFromAPI />
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-
-      <ButtonChangeTheme />
+      <Routing />
     </div>
   );
 }
---
+++ src/Routing.js
@@ -0,0 +1,13 @@
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
+
+function Routing(){
+
+    return(
+        <BrowserRouter>
+            <Routes>             
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default Routing;
