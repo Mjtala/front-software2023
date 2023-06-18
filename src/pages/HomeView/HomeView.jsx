80aeea362b5d67cc27987c423edcdffb2288c866
Mjtala <mjtala@uc.cl>
2023-06-18 12:42:42 -0400

    changes

--- src/pages/HomeView/HomeView.jsx
+++ src/pages/HomeView/HomeView.jsx
@@ -7,28 +7,28 @@ import { useNavigate } from 'react-router-dom';
 function HomeView() {
 
   const navigate = useNavigate();
- 
-return (
-  <>
-  <div  className='background'>
-      <div className='Text'>
-        <div className='Title'>
-          <h1>
-            TeamUp
-          </h1>
-        </div>
-        <div className='subtitle'>
-          <h3>
-            Are you ready for this new experience?
-          </h3>
-        </div>
-        <div className='ContainerButtons'>
-          <ButtonChangePath onClick={()=>navigate("/iniciar_sesion_empresa")}>Arriendo Mi Cancha</ButtonChangePath>
-          <ButtonChangePath onClick={()=>navigate("/iniciar_sesion_jugador")}>Busco Cancha</ButtonChangePath>
+
+  return (
+    <>
+      <div className='background'>
+        <div className='Text'>
+          <div className='Title'>
+            <h1>
+              TeamUp
+            </h1>
+          </div>
+          <div className='subtitle'>
+            <h3>
+              Are you ready for this new experience?
+            </h3>
+          </div>
+          <div className='ContainerButtons'>
+            <ButtonChangePath onClick={() => navigate("/iniciar_sesion_empresa")}>Arriendo Mi Cancha</ButtonChangePath>
+            <ButtonChangePath onClick={() => navigate("/iniciar_sesion_jugador")}>Busco Cancha</ButtonChangePath>
+          </div>
         </div>
       </div>
-    </div>
-    <div  className="full-size-img"></div>
+      <div className="full-size-img"></div>
     </>
   )
 }
