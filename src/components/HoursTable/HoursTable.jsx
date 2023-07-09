f63827791acbde5d41c274b6096fb3a2594b8667
Felipehen <felipe.henriquez@uc.cl>
2023-07-09 18:31:39 -0400

    Update: Rating working
    Ahora los rating funcionan

--- src/components/HoursTable/HoursTable.jsx
+++ src/components/HoursTable/HoursTable.jsx
@@ -1,13 +1,15 @@
 import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
 import "./HoursTable.css"
 import PropTypes from 'prop-types';
 import { useLocalStorage } from 'usehooks-ts';
 import config from '../../config'
 
-function HoursTable( field) {
+function HoursTable(field) {
 
 	const [userConnectedData] = useLocalStorage("UserInfo", null)
+
+	const [success, setSuccess] = useState(false)
 
 	const allHours = Array.from({ length: 16 }, (_, index) => index + 8);
 
@@ -21,9 +23,12 @@ function HoursTable( field) {
 			};
 			console.log(configaxios)
 			const url = `${config.route}player/booking` //TODO:
-			const data = { 'enclousureid': field['id'], 'hour': hour , 'date':field.day}
+			const data = { 'enclousureid': field['id'], 'hour': hour, 'date': field.day }
 			console.log(data)
 			const response = await axios.post(url, data, configaxios)
+			if (response.status === 200) {
+				setSuccess(true)
+			}
 			console.log(response.data, "response.data")
 			console.log(data, "data")
 		} catch (error) {
@@ -33,32 +38,35 @@ function HoursTable( field) {
 
 	const field_modified = field.field
 	return (
-		<div className="DivHours">
-			{field_modified ? (
-				<div>
-					<h4>Horarios</h4>
-					<div className="">
+		<>
+			<div>{success && <h2>Registro con exito</h2>}</div>
+			<div className="DivHours">
 
-						<div key={field_modified.id}>
-							{allHours.map((particularhour) => (
-								<div key={particularhour}>
-									<p>{particularhour}:00 - {particularhour + 1}:00</p>
-									{field_modified && field_modified.playerperhour && field_modified.playerperhour[particularhour] === field_modified.maxplayers ? (
-										<button className="botonHoursTableNo" disabled>Cancha llena</button>
-									) : (
-										<button className="botonHoursTableYes" onClick={() => handleReservarCupo(field_modified, particularhour)}>Registrarse</button>
-									)}
-								</div>
-							))}
+				{field_modified ? (
+					<div>
+						<h4>Horarios</h4>
+						<div className="">
+							<div key={field_modified.id}>
+								{allHours.map((particularhour) => (
+									<div key={particularhour}>
+										<p>{particularhour}:00 - {particularhour + 1}:00</p>
+										{field_modified && field_modified.playerperhour && field_modified.playerperhour[particularhour] === field_modified.maxplayers ? (
+											<button className="botonHoursTableNo" disabled>Cancha llena</button>
+										) : (
+											<button className="botonHoursTableYes" onClick={() => handleReservarCupo(field_modified, particularhour)}>Registrarse</button>
+										)}
+									</div>
+								))}
+							</div>
+
 						</div>
-
+					</div>) : (
+					<div className="DivNoFields">
+						<p>No hay canchas ni horarios que mostrar</p>
 					</div>
-				</div>) : (
-				<div className="DivNoFields">
-					<p>No hay canchas ni horarios que mostrar</p>
-				</div>
-			)}
-		</div>
+				)}
+			</div>
+		</>
 	);
 }
 
--- src/components/ViewPlayer/PlayerProfileRateBookings.jsx
+++ src/components/ViewPlayer/PlayerProfileRateBookings.jsx
@@ -3,72 +3,93 @@ import Select from 'react-select';
 import './PlayerProfileRateBookings.css';
 import PropTypes from 'prop-types';
 import { AiOutlineStar, AiFillStar } from "react-icons/ai"
+import { useLocalStorage } from 'usehooks-ts';
+import config from '../../config'
+import axios from "axios";
 
 const Estrellas = (props) => {
 
-    return (
-        <div className="estrellas">
-            Calificación: {
-                    [... new Array(5)].map((star, index)=>{
-                        return index< props.score ? <AiFillStar key={star}/> : <AiOutlineStar />
-                    })
-                }
-        </div>
-    )
+  return (
+    <div className="estrellas" key={0}>
+      Calificación: {
+        [... new Array(5)].map((star, index) => {
+          return index < props.score ? <AiFillStar key={star} /> : <AiOutlineStar />
+        })
+      }
+    </div>
+  )
 }
 
-export const Modal = ({setModal}) => {
+export const Modal = ({ setModal, fieldid, fieldname }) => {
 
-    const [value, setValue] = useState(0);
-    const [rate, setRate] = useState(false);
+  const [userConnectedData] = useLocalStorage("UserInfo", null)
 
-    useEffect(() => {
-        console.log(value);
-      }, [value]);
+  const [value, setValue] = useState(0);
+  const [rate, setRate] = useState(false);
 
-    const options = [
-        { value: 1, label: '1' },
-        { value: 2, label: '2' },
-        { value: 3, label: '3' },
-        { value: 4, label: '4' },
-        { value: 5, label: '5' }
-      ]
+  useEffect(() => {
+    console.log(value);
+  }, [value]);
 
-      const handleRatingChange = (selectedOption) => {
-        setValue(selectedOption.value);
-        setRate(false)
-      };
-      
-      const Rating = () => (
-        <Select options={options} className="selector" onChange={handleRatingChange} 
-        value={options.find(option => option.value === value)} />
-      )
+  const options = [
+    { value: 1, label: '1' },
+    { value: 2, label: '2' },
+    { value: 3, label: '3' },
+    { value: 4, label: '4' },
+    { value: 5, label: '5' }
+  ]
 
-      const handleRatingBack = () => {
-        setRate(true);
+  const handleRatingChange = (selectedOption) => {
+    setValue(selectedOption.value);
+    setRate(false)
+  };
+
+  const Rating = () => (
+    <Select options={options} className="selector" onChange={handleRatingChange}
+      value={options.find(option => option.value === value)} />
+  )
+
+  const handleRatingBack = async () => {
+    const configaxios = {
+      headers: {
+        "Authorization": userConnectedData.id,
+        withCredentials: true
       }
+    };
+    const url = `${config.route}ratings/enclousures/${fieldid}` //TODO:
+    console.log(url)
+    const data = { 'rating': value }
+    console.log(data)
+    const response = await axios.post(url, data, configaxios)
+    console.log(response.data)
+    setRate(true);
+    console.log(value, fieldid)
+  }
 
-    return (
-        <div className="backshadow">
-            <div className="custom-modal">
-                <Rating value={value} />
-                <button className="send-button" onClick={() => handleRatingBack(value) }>Enviar</button>
-                {rate === true && (<Estrellas score={value}/>)}
-                
-                <div className="delete-icon"
-                onClick={() => setModal(false)}>x</div>
-            </div>
-        </div>
-    )
+  return (
+    <div className="backshadow">
+      <div className="custom-modal">
+        <p>Agregando reseña para la cancha {fieldname}</p>
+        <Rating value={value} />
+        <button className="send-button" onClick={() => handleRatingBack(value)}>Enviar</button>
+        {rate === true && (<Estrellas score={value} />)}
+
+        <div className="delete-icon"
+          onClick={() => setModal(false)}>x</div>
+      </div>
+    </div>
+  )
 }
 
 Modal.propTypes = {
-     setModal: PropTypes.func
-   };
+  setModal: PropTypes.func,
+  fieldname: PropTypes.string,
+  fieldid: PropTypes.number
+};
 
 Estrellas.propTypes = {
-     score: PropTypes.number
- }
+  score: PropTypes.number
+}
 
 export default Modal;
 
